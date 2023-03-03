aaf32ba3895a15864f40224502c7d2bb21fc84c0
skydoves <skydoves@naver.com>
2023-03-03 16:05:45 +0900

    Update video constraints and remove unused tags

--- lesson01/js/main.js
+++ lesson01/js/main.js
@@ -1,23 +1,13 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
 'use strict';
 
 // Put variables in global scope to make them available to the browser console.
 const constraints = window.constraints = {
     audio: false,
-    video: true
+    video: { width: 1280, height: 720 }
 };
 
 function handleSuccess(stream) {
     const video = document.querySelector('video');
-    const videoTracks = stream.getVideoTracks();
-    console.log('Got stream with constraints:', constraints);
-    console.log(`Using video device: ${videoTracks[0].label}`);
     window.stream = stream; // make variable available to browser console
     video.srcObject = stream;
 }
