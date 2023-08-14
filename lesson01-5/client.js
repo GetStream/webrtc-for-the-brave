081ef02a3bdd258ffe4a78a5dcaea88a72dc0a1c
skydoves <skydoves@naver.com>
2023-08-14 17:43:00 +0900

    Implement sdp exchanges for clients

--- lesson01-5/client.js
+++ lesson01-5/client.js
@@ -1,4 +1,4 @@
-import { io } from "socket.io-client"
+import { io } from "socket.io-client" // const io = require('socket.io-client');
 
 const socket = io('http://localhost:3000'); // Replace with your server's URL
 const pc_config = {
@@ -54,3 +54,64 @@ const createOffer = () => {
             console.log(error);
         });
 };
+
+const createAnswer = (sdp) => {
+    newPC.setRemoteDescription(new RTCSessionDescription(sdp)).then(() => {
+        console.log("answer set remote description success");
+        peerConnection
+            .createAnswer({
+                offerToReceiveVideo: true,
+                offerToReceiveAudio: true,
+            })
+            .then(sdp1 => {
+                console.log("create answer");
+                peerConnection.setLocalDescription(new RTCSessionDescription(sdp1));
+                socket.emit("answer", sdp1);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    });
+};
+
+function renderVideo() {
+    navigator.mediaDevices
+        .getUserMedia({
+            video: true,
+            audio: true,
+        })
+        .then(stream => {
+            if (localVideoRef.current) localVideoRef.current.srcObject = stream;
+
+            // 자신의 video, audio track을 모두 자신의 RTCPeerConnection에 등록한다.
+            stream.getTracks().forEach(track => {
+                newPC.addTrack(track, stream);
+            });
+            newPC.onicecandidate = e => {
+                if (e.candidate) {
+                    console.log("onicecandidate");
+                    newSocket.emit("candidate", e.candidate);
+                }
+            };
+            newPC.oniceconnectionstatechange = e => {
+                console.log(e);
+            };
+
+            newPC.ontrack = ev => {
+                console.log("add remotetrack success");
+                if (remoteVideoRef.current)
+                    remoteVideoRef.current.srcObject = ev.streams[0];
+            };
+
+            // 자신의 video, audio track을 모두 자신의 RTCPeerConnection에 등록한 후에 room에 접속했다고 Signaling Server에 알린다.
+            // 왜냐하면 offer or answer을 주고받을 때의 RTCSessionDescription에 해당 video, audio track에 대한 정보가 담겨 있기 때문에
+            // 순서를 어기면 상대방의 MediaStream을 받을 수 없음
+            newSocket.emit("join_room", {
+                room: "1234",
+                email: "sample@naver.com",
+            });
+        })
+        .catch(error => {
+            console.log(`getUserMedia error: ${error}`);
+        });
+}
